cb887d7642b49b9457c484def976435659f90d7b
Stone Sha <stoneshaus@gmail.com>
2024-07-18 08:15:25 -0700

    added DATABASE_URL env variable loading

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # grocery-tracker-backend
 
-Run `go install` to install all dependencies. Then run `go run main.go` to start it on [localhost:8080](localhost:8080).
+Run `go run main.go` to automatically install dependencies and start it on [localhost:8080](localhost:8080).
--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
-  "net/http"
-
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"net/http"
+	"os"
 )
 
+func init() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+	if DATABASE_URL == "" {
+		panic("DATABASE_URL environment variable is not set. Exiting.")
+	}
+}
+
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
