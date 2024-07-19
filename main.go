fbb0c7b714eaba7ebb4f04bdae0713b7505fd78b
Stone Sha <stoneshaus@gmail.com>
2024-07-18 17:14:08 -0700

    clean up code

--- main.go
+++ main.go
@@ -3,38 +3,51 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	models "github.com/koshigi/grocery-tracker-backend/db/models"
+	models "github.com/koshigi/grocery-tracker-backend/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"net/http"
 	"os"
 )
 
-func init() {
-	err := godotenv.Load(".env")
+var (
+	server       *gin.Engine
+	DATABASE_URL string
+	DB           *gorm.DB
+)
 
-	if err != nil {
+func init() {
+	// load environment variables
+	env_err := godotenv.Load(".env")
+
+	if env_err != nil {
 		panic("Error loading .env file")
 	}
 
-	DATABASE_URL := os.Getenv("DATABASE_URL")
+	DATABASE_URL = os.Getenv("DATABASE_URL")
 	if DATABASE_URL == "" {
 		panic("DATABASE_URL environment variable is not set. Exiting.")
 	}
-}
 
-func main() {
-	//Creation of a gin router with default middlewware
-	r := gin.Default()
+	// set port
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8080"
+	}
+	os.Setenv("PORT", PORT)
 
-	DATABASE_URL := os.Getenv("DATABASE_URL")
-	db, err := gorm.Open(postgres.Open(DATABASE_URL), &gorm.Config{})
-	if err != nil {
+	var db_err error
+	DB, db_err = gorm.Open(postgres.Open(DATABASE_URL), &gorm.Config{})
+	if db_err != nil {
 		panic("Couldn't connect to database.")
 	}
 
+	server = gin.Default()
+}
+
+func main() {
 	//Stone's initial test Get enpoint
-	r.GET("/ping", func(c *gin.Context) {
+	server.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
@@ -43,16 +56,16 @@ func main() {
 	// Placeholder for endpoints until someone figures out what is post, put, get
 
 	// Creating the List parent object
-	r.POST("/createList", func(c *gin.Context) {
+	server.POST("/createList", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "create list test",
 		})
 	})
 
 	// Retrieving the list and its children
-	r.GET("/getList/:id/", func(c *gin.Context) {
+	server.GET("/getList/:id/", func(c *gin.Context) {
 		id := c.Param("id")
-		result := db.First(&models.List{}, id)
+		result := DB.First(&models.List{}, id)
 
 		//TODO: fix this error Error #01: json: unsupported type: func() time.Time
 		// unable to marshal the time data type, maybe specify in models?
@@ -62,19 +75,19 @@ func main() {
 	})
 
 	// Deleting a list
-	r.DELETE("/deleteList", func(c *gin.Context) {
+	server.DELETE("/deleteList", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "delete list test",
 		})
 	})
 
 	// Update items to list
-	r.POST("/updateItems", func(c *gin.Context) {
+	server.POST("/updateItems", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "update item test",
 		})
 	})
 
 	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-	r.Run()
+	server.Run()
 }
